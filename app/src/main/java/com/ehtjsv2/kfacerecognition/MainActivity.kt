f69548374d4def4458bd8a75a0360334d6cffdf1
ehtjsv2 <ehtjsv2@naver.com>
2023-06-06 01:17:29 +0900

    add module

---
+++ app/src/main/java/com/ehtjsv2/kfacerecognition/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ehtjsv2.kfacerecognition
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
